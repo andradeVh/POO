b8e27d804bac38ecd3b9aa6022e62bb988109c7d
andradeVh <andrade.vh18@gmail.com>
2026-02-27 16:07:19 -0300

    Adicionando exercício 01

--- lab-02/Exercicio01/src/main/java/Main.java
+++ lab-02/Exercicio01/src/main/java/Main.java
@@ -1,24 +1,29 @@
 public class Main {
 
-
     static void main() {
 
         int dia = Integer.parseInt(IO.readln("Entre com o dia: "));
-        String mes = IO.readln("Entre com o mês: ");
+        String mes = IO.readln("Entre com o mês: ").toLowerCase();
 
-        if ((mes.equals("dezembro") && dia > 20 ) || mes.equals("janeiro") || mes.equals("fevereiro") || (mes.equals("março") && dia < 20)){
+        if (dia > 31 || dia < 1) {
+            IO.println("Dia inválido");
+            return;
+
+        } else if ((mes.equals("dezembro") && dia > 20) || mes.equals("janeiro") || mes.equals("fevereiro")
+                || (mes.equals("março") && dia < 20)) {
             IO.println("Verão");
-        }
-        if (mes.equals("março") || mes.equals("abril") || mes.equals("maio") || (mes.equals("junho") && dia < 22)){
+        } else if ((mes.equals("março") && dia >= 20) || mes.equals("abril") || mes.equals("maio")
+                || (mes.equals("junho") && dia < 20)) {
             IO.println("Outono");
-        }
-        if (mes.equals("março") || mes.equals("abril") || mes.equals("maio") || (mes.equals("junho") && dia < 22)){
+        } else if ((mes.equals("junho") && dia >= 20) || mes.equals("julho") || mes.equals("agosto")
+                || (mes.equals("setembro") && dia < 22)) {
             IO.println("Inverno");
-        }
-        if (mes.equals("março") || mes.equals("abril") || mes.equals("maio") || (mes.equals("junho") && dia < 22)){
+        } else if (mes.equals("setembro") || mes.equals("outubro") || mes.equals("novembro")
+                || (mes.equals("dezembro") && dia < 22)) {
             IO.println("Primavera");
+        } else {
+            IO.println("Mês inválido");
         }
-
 
     }
 }
