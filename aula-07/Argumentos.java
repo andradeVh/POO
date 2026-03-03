012b88f1614271e41a3f53f0d4cb9c95d540a4e2
Victor Hugo Andrade <andrade.vh18@gmail.com>
2026-03-03 23:14:15 +0000

    Adicionando fori

--- aula-07/Argumentos.java
+++ aula-07/Argumentos.java
@@ -1,10 +1,15 @@
 public class Argumentos{
     public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            
+        
         if (args.length == 0){
             IO.println("Não forneceu argumentos de linha de comando");
         }else{
-            IO.println("Primeiro argumento: " + args[0]);
+            IO.println("Argumento número "+ (i + 1) +" : "+ args[i]);
         }
-        
+    }
+
     }
 }
