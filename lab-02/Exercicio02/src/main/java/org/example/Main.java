498718e838a2e7ac3d14b5f510713a67edb9a246
andradeVh <andrade.vh18@gmail.com>
2026-03-04 01:25:15 -0300

    Exercício 02 concluído

--- lab-02/Exercicio02/src/main/java/org/example/Main.java
+++ lab-02/Exercicio02/src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        Random gerador = new Random();
+        int numeroAleatorio = gerador.nextInt(100);
+        int palpite;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+
+        do {
+            palpite = Integer.parseInt(IO.readln("Insira seu palpite: "));
+            if (palpite > numeroAleatorio) {
+                IO.println("O seu palpite é maior que o valor");
+            } else {
+                IO.println("O seu palpite é menor que o valor");
+            }
+        } while (palpite != numeroAleatorio);
+        IO.println("Parabéns, você acertou!");
     }
 }
+
