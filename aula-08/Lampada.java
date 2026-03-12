92f4cdb58e6ee4c6ec5dbc53733bb086d3618f5f
Victor Hugo Andrade <andrade.vh18@gmail.com>
2026-03-12 20:58:58 +0000

    Adicionando aula 08

---
+++ aula-08/Lampada.java
@@ -0,0 +1,19 @@
+public class Lampada {
+
+    // Atributos
+    private boolean ligada;
+
+    // Métodos
+
+    boolean isLigada(){
+        return ligada;
+    }
+
+    boolean trocaEstado(){
+        ligada = !ligada;
+        return ligada;
+    }
+
+    //obs: métodos são publicos, atributos são privados
+
+}
---
+++ aula-08/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        static void main(String[] args) {
+
+                // Utilizando o metodo Lampada
+//              Lampada a = new Lampada();
+//              Lampada b = new Lampada();
+//
+//              a.trocaEstado();
+//
+//              System.out.println(a.isLigada());
+//              System.out.println(b.isLigada());
+        }
+}
