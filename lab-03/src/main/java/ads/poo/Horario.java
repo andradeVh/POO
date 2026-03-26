cac9f6413638a44b4c133330e9dd1e4cb2d9b836
andradeVh <andrade.vh18@gmail.com>
2026-03-26 15:32:40 -0300

    Adicionando unidade por extenso em horários.

--- lab-03/src/main/java/ads/poo/Horario.java
+++ lab-03/src/main/java/ads/poo/Horario.java
@@ -27,10 +27,10 @@ public class Horario {
         this.segundo = segundo;
     }
 
-    public boolean setHora(int a){
+    public boolean setHora(int a) {
         boolean horaCerta;
         hora = a;
-        if (hora > 24){
+        if (hora > 24) {
             horaCerta = false;
         } else {
             horaCerta = true;
@@ -38,10 +38,10 @@ public class Horario {
         return horaCerta;
     }
 
-    public boolean setMinuto(int a){
+    public boolean setMinuto(int a) {
         boolean minutoCerto;
         minuto = a;
-        if (minuto > 60){
+        if (minuto > 60) {
             minutoCerto = false;
         } else {
             minutoCerto = true;
@@ -49,10 +49,10 @@ public class Horario {
         return minutoCerto;
     }
 
-    public boolean setSegundo(int a){
+    public boolean setSegundo(int a) {
         boolean segundoCerto;
         segundo = a;
-        if (segundo > 60){
+        if (segundo > 60) {
             segundoCerto = false;
         } else {
             segundoCerto = true;
@@ -64,4 +64,56 @@ public class Horario {
     public String toString() {
         return hora + ":" + minuto + ":" + segundo;
     }
+
+    public String porExtenso() {
+        return numeroHoraPorExtenso(hora) + " " + unidadeTempo(hora, "hora", "horas") + ", " + numeroPorExtenso(minuto) + " " + unidadeTempo(minuto, "minuto", "minutos") + " e " + numeroPorExtenso(segundo) + " " + unidadeTempo(segundo, "segundo", "segundos");
+    }
+
+    private String numeroHoraPorExtenso(int valor) {
+        if (valor == 1) {
+            return "uma";
+        }
+        if (valor == 2) {
+            return "duas";
+        }
+        return numeroPorExtenso(valor);
+    }
+
+    private String numeroPorExtenso(int valor) {
+        return switch (valor) {
+            case 0 -> "zero";
+            case 1 -> "um";
+            case 2 -> "dois";
+            case 3 -> "tres";
+            case 4 -> "quatro";
+            case 5 -> "cinco";
+            case 6 -> "seis";
+            case 7 -> "sete";
+            case 8 -> "oito";
+            case 9 -> "nove";
+            case 10 -> "dez";
+            case 11 -> "onze";
+            case 12 -> "doze";
+            case 13 -> "treze";
+            case 14 -> "quatorze";
+            case 15 -> "quinze";
+            case 16 -> "dezesseis";
+            case 17 -> "dezessete";
+            case 18 -> "dezoito";
+            case 19 -> "dezenove";
+            case 20 -> "vinte";
+            case 30 -> "trinta";
+            case 40 -> "quarenta";
+            case 50 -> "cinquenta";
+            default -> numeroPorExtenso((valor / 10) * 10) + " e " + numeroPorExtenso(valor % 10);
+        };
+    }
+
+    private String unidadeTempo(int valor, String singular, String plural) {
+        if (valor == 1) {
+            return singular;
+        }
+        return plural;
+    }
+
 }
