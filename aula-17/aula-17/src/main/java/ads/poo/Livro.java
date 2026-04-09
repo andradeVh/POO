00a67b94eec53887786e7dfb3497bbaa8bef087b
andradeVh <andrade.vh18@gmail.com>
2026-04-09 20:21:10 -0300

    Adicionando aula-17

---
+++ aula-17/aula-17/src/main/java/ads/poo/Livro.java
@@ -0,0 +1,43 @@
+package ads.poo;
+
+public class Livro {
+    private final String ISBN;
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String titulo, String ISBN, String autor, int ano) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
---
+++ aula-17/aula-17/src/main/java/ads/poo/Main.java
@@ -0,0 +1,94 @@
+package ads.poo;
+
+import java.util.HashMap;
+
+public class Main {
+
+    private HashMap<String, Livro> listaLivros;
+
+    public Main() {
+        // No construtor dessa classe já crie 4 livros e adicione no acervo
+        this.listaLivros = new HashMap<>();
+        listaLivros.put("123.123.123", new Livro("a", "123.123.123", "Juca", 1920));
+        listaLivros.put("123.123.124", new Livro("b", "123.123.123", "Maria", 2020));
+        listaLivros.put("123.123.125", new Livro("c", "123.123.123", "Joao", 1623));
+        listaLivros.put("123.123.126", new Livro("d", "123.123.123", "Josué", 1822));
+
+
+        // pessoas.put("123", new Pessoa("Juca", 35));
+    }
+
+
+    void menu() {
+        int opcao;
+        do {
+            IO.println();
+            opcao = Integer.parseInt(IO.readln("""
+                    ..:: Menu ::..
+                     1 - Cadastrar conta
+                     2 - Listar contas
+                     3 - Depositar
+                     4 - Sacar
+                     5 - Sair
+                    """));
+
+            switch (opcao) {
+                case 1:
+                    cadastrar();
+                    break;
+                case 2:
+                    // listarLivros();
+                    break;
+                case 3:
+                    // listarLivrosAno();
+                    break;
+                case 4:
+                    // alterarDadosLivro();
+                    break;
+                case 5:
+                    IO.println("Encerrando sistema!");
+                    break;
+                default:
+                    IO.println("Opção inválida. Tente novamente");
+                    break;
+            }
+        } while (opcao != 5);
+    }
+
+    void cadastrar() {
+        var titulo = IO.readln("Título do livro: ");
+        var autor = IO.readln("Autor do livro: ");
+        var ISBN = IO.readln("ISBN do livro: ");
+        var ano = Integer.parseInt(IO.readln("Ano de publicação: "));
+
+        /* Criando uma conta nova */
+        Livro livro = new Livro(titulo, autor, ISBN, ano);
+
+        /* Guardando a conta dentro da lista */
+        listaLivros.put(ISBN, new Livro(titulo, ISBN, autor, ano));
+
+        IO.println("Livro cadastrado com sucesso!");
+    }
+/*
+    void listarLivros(){
+        listaLivros.forEach((ISBN, titulo  ) -> {
+            System.out.println("ISBN: " + ISBN);
+            System.out.println("Título: " + listaLivros.get());
+        });
+    }
+*/
+
+    static void main(String[] args) {
+
+        Main main = new Main();
+        main.menu();
+        // Sistema de uma livraria para gestão de acervo
+        // 1 - Cadastrar livro
+        // 2 - Listar todos os livros
+        // 3 - Listar todos os livros publicados em um ano específico
+        // 4 - Alterar dados de um livro
+        // 5 - Sair
+        }
+
+}
+
