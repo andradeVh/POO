03c46a9935844f4a4a3a9af515a89c88e42c067a
andradeVh <andrade.vh18@gmail.com>
2026-04-30 16:24:30 -0300

    Adicionando método gerenciar contato

--- lab-03/src/main/java/ads/poo/Main.java
+++ lab-03/src/main/java/ads/poo/Main.java
@@ -1,10 +1,14 @@
 package ads.poo;
+
+import java.lang.classfile.constantpool.FieldRefEntry;
+
 public class Main {
     static void main() {
         Ponto a = new Ponto(1, 2);
         Ponto b = new Ponto(2, 2);
         Horario h = new Horario(1, 1, 1);
         Horario j = new Horario(1, 1, 2);
+        Retangulo r = new Retangulo();
 
         double distancia = a.getDistancia(b);
         long distanciaH = j.diferencaHora(h);
@@ -14,5 +18,12 @@ public class Main {
         System.out.println(h.porExtenso());
         System.out.println(h.tempoSegundos());
         System.out.println(distanciaH);
+
+        r.setAltura(15);
+        r.setLargura(15);
+
+        System.out.println(r.toString());
+
+
     }
 }
--- lab-03/src/main/java/ads/poo/Retangulo.java
+++ lab-03/src/main/java/ads/poo/Retangulo.java
@@ -8,12 +8,53 @@ public class Retangulo {
         this.largura = 4;
     }
 
-    public Retangulo(int altura, int largura, int tipoCodificacao){
+    public Retangulo(int altura, int largura, int tipoCodificacao) {
         this();
-        if (altura < 0 || largura < 0){
+        if (altura < 2 || largura < 2) {
             this.altura = altura;
             this.largura = largura;
         }
 
     }
+
+    public int getAltura(){
+        return altura;
+    }
+
+    public int getLargura(){
+        return largura;
+    }
+
+    public int getArea() {
+        return altura * largura;
+    }
+
+    public int getPerimetro() {
+        return 2 * altura + 2 * largura;
+    }
+
+    public void setAltura(int altura){
+        this.altura = altura;
+    }
+
+    public void setLargura(int largura){
+        this.largura = largura;
+    }
+
+    @Override
+    public String toString() {
+        if (largura < 2 || altura < 2) return "+";
+
+        final StringBuilder sb = new StringBuilder();
+        String cima = "+" + "-".repeat(largura - 2) + "+\n";
+        sb.append(cima);
+
+        String meio = "|" + " ".repeat(largura - 2) + "|\n";
+        sb.append(meio.repeat(altura - 2));
+
+        String baixo = "+" + "-".repeat(largura - 2) + "+\n";
+        sb.append(baixo);
+        return sb.toString();
+    }
+
 }
